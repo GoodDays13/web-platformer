a6622c4eb54deb95f66aee0a136ad33cb7a854f0
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-03-27 18:45:20 -0400

    Extract out constants

--- backend/game.py
+++ backend/game.py
@@ -1,9 +1,7 @@
 import asyncio
 from dataclasses import dataclass
 
-from constants import CANVAS_W
-
-GRAVITY = 4096
+from constants import CANVAS_W, GRAVITY, PLAYER_SPEED, PLAYER_ACCELERATION, JUMP_VELOCITY, JUMP_DURATION, COYOTE_TIME, PLAYER_SIZE
 
 class Game:
     player: GameObject
@@ -15,7 +13,7 @@ class Game:
     jumping: bool
 
     def __init__(self) -> None:
-        self.player = GameObject(0, 0, 0, 0, 32, 32)
+        self.player = GameObject(0, 0, 0, 0, PLAYER_SIZE, PLAYER_SIZE)
         self.platforms = [
             GameObject(0, 64, 0, 0, 32, 32),
             GameObject(256, 128, 0, 0, 64, 32)
@@ -33,7 +31,7 @@ class Game:
             if down and (self.grounded or self.coyote):
                 self.jumping = True
                 self.grounded = False
-                asyncio.get_event_loop().call_later(.15, self.stop_jump)
+                asyncio.get_event_loop().call_later(JUMP_DURATION, self.stop_jump)
             else:
                 self.jumping = False
 
@@ -42,57 +40,50 @@ class Game:
 
         velocity_target_x = 0
         if 'KeyA' in self.held_keys:
-            velocity_target_x -= 512
+            velocity_target_x -= PLAYER_SPEED
         if 'KeyD' in self.held_keys:
-            velocity_target_x += 512
+            velocity_target_x += PLAYER_SPEED
 
         velocity_diff = velocity_target_x - self.player.velocity_x
         if velocity_diff != 0:
             sign = abs(velocity_diff) / velocity_diff
             velocity_diff = abs(velocity_diff)
-            self.player.velocity_x += sign * min(velocity_diff, 3072 * delta_time)
+            self.player.velocity_x += sign * min(velocity_diff, PLAYER_ACCELERATION * delta_time)
 
-        # Set velocity based on either jumping or gravity
         if self.jumping:
-            self.player.velocity_y = 512
+            self.player.velocity_y = JUMP_VELOCITY
         else:
-            self.player.velocity_y -= GRAVITY * delta_time # gravity
+            self.player.velocity_y -= GRAVITY * delta_time
 
         prev_x = self.player.x
         prev_y = self.player.y
 
-        # Move based on velocity
         self.player.x += self.player.velocity_x * delta_time
         self.player.y += self.player.velocity_y * delta_time
 
         self.grounded = False
 
-        # Absolute floor collision
         if self.player.y < 0:
             self.player.y = 0
             self.grounded = True
             self.player.velocity_y = 0
 
-        # Platform collisions
         for platform in self.platforms:
             if overlaps(self.player, platform):
-                # Which side did we enter from?
-                # Check where the self.player was BEFORE this tick
-                if prev_y >= platform.y + platform.height:  # was above, entered from top
-                    self.player.y = platform.y + platform.height  # push back up
+                if prev_y >= platform.y + platform.height:
+                    self.player.y = platform.y + platform.height
                     self.player.velocity_y = 0
                     self.grounded = True
-                    self.coyote = .05
-                elif prev_y + self.player.height <= platform.y:  # was below, entered from bottom
+                    self.coyote = COYOTE_TIME
+                elif prev_y + self.player.height <= platform.y:
                     self.player.y = platform.y - self.player.height
                     self.player.velocity_y = 0
                     self.jumping = False
-                elif prev_x + self.player.width <= platform.x:  # left side
+                elif prev_x + self.player.width <= platform.x:
                     self.player.x = platform.x - self.player.width
-                elif prev_x >= platform.x + platform.width:  # right side
+                elif prev_x >= platform.x + platform.width:
                     self.player.x = platform.x + platform.width
 
-        # Absolute walls collision
         if self.player.x < 0:
             self.player.x = 0
             self.player.velocity_x = 0
@@ -101,13 +92,11 @@ class Game:
             self.player.velocity_x = 0
 
 
+# y is up, (x, y) is bottom-left
 @dataclass
 class GameObject:
-    # left
     x: float
-    # bottom
     y: float
-    # velocity
     velocity_x: float
     velocity_y: float
     width: float
