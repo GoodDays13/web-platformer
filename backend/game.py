9b8195d6261f3765d2ebbc1d04388b439ba9a431
GoodDays13 <42877449+GoodDays13@users.noreply.github.com>
2026-03-28 09:24:22 -0400

    Add more platforms

--- backend/game.py
+++ backend/game.py
@@ -22,6 +22,11 @@ class Game:
             GameObject(544, 64 * 3, 0, 0, 32, 32),
             GameObject(544 + 64, 64 * 4.5, 0, 0, 32, 32),
             GameObject(544 + 64 * 2, 64 * 6, 0, 0, 32, 32),
+            GameObject(CANVAS_W - 16, 64 * 7.5, 0, 0, 32, 32),
+            GameObject(576, 64 * 9, 0, 0, 32, 32),
+            GameObject(576 - 64, 64 * 11, 0, 0, 32 * 3, 32),
+            GameObject(256, 64 * 10, 0, 0, 64, 32),
+            GameObject(576 - 64, 64 * 12.5, 0, 0, 32 * 3, 32),
         ]
         self.camera_y = 0
         self.held_keys = set()
